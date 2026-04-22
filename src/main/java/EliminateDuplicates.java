709bd7cf7972edcdb4301505e379c88ed2477f23
Carlos Montes <carloss.montes@gmail.com>
2026-04-22 12:44:43 -0600

    Add EliminateDuplicates

---
+++ src/main/java/EliminateDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class EliminateDuplicates {
+
+    public static Integer[] eliminateDuplicates(Integer[] array) {
+
+        // Convert array to LinkedHashSet to eliminate duplicates while preserving insertion order
+        Set<Integer> setSinDuplicados = new LinkedHashSet<>(Arrays.asList(array)); 
+        // Convert the set back to an array of Integer type
+        Integer[] arregloSinDuplicados = setSinDuplicados.toArray(new Integer[0]);
+
+        return arregloSinDuplicados;
+    }
+
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 2, 4, 5, 1, 6, 3};
+
+        System.out.println(Arrays.toString(eliminateDuplicates(array)));
+    }
+}
